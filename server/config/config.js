2cd30d987ca032470b86ad7137d80472995b895d
mauro galindez <galindez.m@opendevpro.com>
2020-05-09 17:15:21 -0300

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mauro:jenny1942@mauro-npa2c.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
